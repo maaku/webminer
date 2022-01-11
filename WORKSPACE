68e3a9f0053d21c96a293291d1af2116d20578eb
Mark Friedenbach <mark@friedenbach.org>
2022-01-11 15:25:42 -0800

    Initial import.

---
+++ WORKSPACE
@@ -0,0 +1,17 @@
+# Copyright (c) 2022 Mark Friedenbach
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+
+# absl source code repository
+git_repository(
+    name = "com_google_absl",
+    remote = "https://github.com/abseil/abseil-cpp.git",
+    commit = "215105818dfde3174fe799600bb0f3cae233d0bf",
+    shallow_since = "1635953174 -0400",
+)
+
+# End of File
---
+++ main.cc
@@ -0,0 +1,26 @@
+// Copyright (c) 2022 Mark Friedenbach
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <iostream>
+
+#include <string>
+#include <vector>
+
+#include "absl/flags/parse.h"
+#include "absl/flags/usage.h"
+
+#include "absl/strings/str_cat.h"
+
+int main(int argc, char **argv)
+{
+    absl::SetProgramUsageMessage(absl::StrCat("Webcash mining daemon.\n", argv[0]));
+
+    absl::ParseCommandLine(argc, argv);
+
+    return 0;
+}
+
+// End of File
