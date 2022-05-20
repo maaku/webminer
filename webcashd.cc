008849db5f90fd10f90a5f5b4d7a38d34f202bb3
Mark Friedenbach <mark@friedenbach.org>
2022-05-20 11:16:40 -0700

    Add webcashd, a C++ implementation of the webcash server process.

---
+++ webcashd.cc
@@ -0,0 +1,20 @@
+// Copyright (c) 2022 Mark Friedenbach
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "absl/flags/parse.h"
+#include "absl/flags/usage.h"
+
+#include "absl/strings/str_cat.h"
+
+int main(int argc, char **argv)
+{
+    absl::SetProgramUsageMessage(absl::StrCat("Webcash server process.\n", argv[0]));
+    absl::ParseCommandLine(argc, argv);
+
+    return 0;
+}
+
+// End of File
